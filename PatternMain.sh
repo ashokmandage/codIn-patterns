7e08a4c9768d22c936019aac133ac744e6b33231
AshokA <ashok.mandage22@gmail.com>
2020-08-18 20:27:06 +0530

    first commit

---
+++ PatternMain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+echo "enter your usrname: "
+read usrname
+testFname="^[A-Z]{3,}"
+if [[ $usrname =~ $testFname ]]
+then
+echo "valid first name"
+else
+echo "invalid first name"
+fi
